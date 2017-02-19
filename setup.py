80e3e5d5879b83091f7db3bbdd5b0fc691f0e52e
Tyler Shimko <TShimko126@gmail.com>
2017-02-19 13:35:41 -0800

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="geppetto-cli",
+      version="0.1",
+      description="Command line interface to automate control of pneumatic setup",
+      url="https://github.com/FordyceLab/geppetto-cli",
+      author="Tyler Shimko",
+      author_email="tshimko@stanford.edu",
+      license="MIT",
+      packages=["geppetto-cli"],
+      zip_safe=False)
